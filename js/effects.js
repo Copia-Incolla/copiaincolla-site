5b4539dee08d13f0797f0d3162a7d9683c744bcc
ABeltramo <beltramo.ale@gmail.com>
2016-01-04 01:11:01 +0100

    Android js hack. Now it's working only on Android

--- js/effects.js
+++ js/effects.js
@@ -1,17 +1,32 @@
 $(".scrollto").on('click', function(e) {
- 
-        target = $(this).attr("href");
-       // prevent default anchor click behavior
-       e.preventDefault();
- 
-       // animate
-       $('html, body').animate({
-           scrollTop: $(target).offset().top
-         }, 500, function(){
- 
-           // when done, add hash to url
-           // (default click behaviour)
-           window.location.hash = target;
-         });
- 
-    });
+    target = $(this).attr("href");
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(target).offset().top
+     }, 500, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = target;
+     });
+
+});
+
+/* ANDROID HACK 
+ * see: http://stackoverflow.com/questions/24944925/background-image-jumps-when-address-bar-hides-ios-android-mobile-chrome
+ */
+$(function(){
+  if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+    jQuery(window).resize("resizeBackground");
+    resizeBackground();
+  }
+  
+  function resizeBackground(){
+    var height = jQuery(window).height() + 60;
+    $(".bigContainer").css("height",height+"px");
+  }
+});
+/* //ANDROID HACK */
