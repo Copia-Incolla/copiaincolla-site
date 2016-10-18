2a5a635cfc7f543649b09c87a738e52f40e0af91
ABeltramo <beltramo.ale@gmail.com>
2016-10-18 13:41:54 +0200

    Modificata pagina dei contatti

--- js/GMaps.js
+++ js/GMaps.js
@@ -1,24 +1,34 @@
-var myCenter=new google.maps.LatLng(45.9390656,8.57494);
+var posizioni = [];
+posizioni["CopiaVerbania"] = new google.maps.LatLng(45.9390656,8.57494);
+posizioni["CopiaArona"] = new google.maps.LatLng(45.7570879,8.5576672);
+posizioni["CopiaTerni"] = new google.maps.LatLng(42.5598042,12.6451605);
+posizioni["CopiaOmegna"] = new google.maps.LatLng(45.8779096,8.4064306);
+posizioni["CopiaCastelFranco"] = new google.maps.LatLng(45.6831699,11.9400613);
 var marker;
-google.maps.event.addDomListener(window, 'load', initializeGMap);
+google.maps.event.addDomListener(window, 'load', initializeGMap("CopiaVerbania"));
 
 
-function initializeGMap(){
+function initializeGMap(posizione){
     var mapProp = {
-      center:myCenter,
+      center:posizioni[posizione],
       zoom:16,
       mapTypeId:google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: false,
-      scrollwheel: false, 
+      scrollwheel: false,
       draggable: false
     };
 
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
     var marker=new google.maps.Marker({
-      position:myCenter,
+      position:posizioni[posizione],
       animation:google.maps.Animation.DROP
     });
 
     marker.setMap(map);
-  }
+}
+
+$(".positionPointer").click(function(e){
+  var clicked = e.target;
+  initializeGMap($(clicked).attr("pos"));
+})
