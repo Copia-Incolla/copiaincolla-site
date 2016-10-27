ae4e52f6064de66c4e67aecae36baeaccab9230c
Ale <ale@localhost.localdomain>
2016-10-27 13:39:50 +0200

    Updated names

--- js/GMaps.js
+++ js/GMaps.js
@@ -1,21 +1,21 @@
 var map,infowindow;
 var posizioni = {
-  "CopiaVerbania"     : [new google.maps.LatLng(45.9390656,8.57494), 			"<b>Copia&Incolla</b> di Verbania"],
-  "CopiaArona"        : [new google.maps.LatLng(45.7570879,8.5576672), 		"<b>Copia&Incolla</b> di Arona"],
-  "CopiaTerni"        : [new google.maps.LatLng(42.5598042,12.6451605), 	"<b>Copia&Incolla</b> di Terni"],
-  "CopiaOmegna"       : [new google.maps.LatLng(45.8779096,8.4064306), 		"<b>Copia&Incolla</b> di Omegna"],
-  "CopiaCastelFranco" : [new google.maps.LatLng(45.6831699,11.9400613), 	"<b>Copia&Incolla</b> di Castel Franco Veneto"],
-	"CornerAzzate"			: [new google.maps.LatLng(45.7773975,8.7965561), 		"<b>Corner C&I</b> presso Proline SAS"],
-	"CornerMagenta"			: [new google.maps.LatLng(45.464511,8.8825773), 		"<b>Corner C&I</b> presso Orta Telefonia"],
-	"CornerGravellona"	: [new google.maps.LatLng(45.9251746,8.4286457), 		"<b>Corner C&I</b> presso Ecotoner"],
-	"CornerCassano"			: [new google.maps.LatLng(45.6713496,8.8194182), 		"<b>Corner C&I</b> presso Foto Carluccia"],
-	"CornerCastano"			: [new google.maps.LatLng(45.5514959,8.7752627), 		"<b>Corner C&I</b> presso CentoxCento phone"],
-	"CornerLegnano"			: [new google.maps.LatLng(45.5955856,8.9142063), 		"<b>Corner C&I</b> presso CentoxCento phone"],
-	"CornerArluno"			: [new google.maps.LatLng(45.5053578,8.942398), 		"<b>Corner C&I</b> presso Mastersi"],
-	"CornerVodIntra"		: [new google.maps.LatLng(45.9362233,8.569285), 		"<b>Corner C&I</b> presso Vodafone (Intra)"],
-	"CornerVodDomo"			: [new google.maps.LatLng(46.099462,8.2850653), 		"<b>Corner C&I</b> presso Vodafone (Domodossola)"],
-	"CornerVodArona"		: [new google.maps.LatLng(45.7602917,8.5593297), 		"<b>Corner C&I</b> presso Vodafone (Arona)"],
-	"CornerVodGrave"		: [new google.maps.LatLng(45.9314662,8.4367192), 		"<b>Corner C&I</b> presso Vodafone (Gravellona)"]
+  "CopiaVerbania"     : [new google.maps.LatLng(45.9390656,8.57494), 			"<b>Copia&Incolla Store</b><br> di Verbania"],
+  "CopiaArona"        : [new google.maps.LatLng(45.7570879,8.5576672), 		"<b>Copia&Incolla Store</b><br> di Arona"],
+  "CopiaTerni"        : [new google.maps.LatLng(42.5598042,12.6451605), 	"<b>Copia&Incolla Store</b><br> di Terni"],
+  "CopiaOmegna"       : [new google.maps.LatLng(45.8779096,8.4064306), 		"<b>Copia&Incolla Store</b><br> di Omegna"],
+  "CopiaCastelFranco" : [new google.maps.LatLng(45.6831699,11.9400613), 	"<b>Copia&Incolla Store</b><br> di Castelfranco Veneto"],
+	"CornerAzzate"			: [new google.maps.LatLng(45.7773975,8.7965561), 		"<b>Copia&Incolla Corner</b><br> presso Proline SAS"],
+	"CornerMagenta"			: [new google.maps.LatLng(45.464511,8.8825773), 		"<b>Copia&Incolla Corner</b><br> presso Orta Telefonia"],
+	"CornerGravellona"	: [new google.maps.LatLng(45.9251746,8.4286457), 		"<b>Copia&Incolla Corner</b><br> presso Ecotoner"],
+	"CornerCassano"			: [new google.maps.LatLng(45.6713496,8.8194182), 		"<b>Copia&Incolla Corner</b><br> presso Foto Carluccia"],
+	"CornerCastano"			: [new google.maps.LatLng(45.5514959,8.7752627), 		"<b>Copia&Incolla Corner</b><br> presso CentoxCento phone"],
+	"CornerLegnano"			: [new google.maps.LatLng(45.5955856,8.9142063), 		"<b>Copia&Incolla Corner</b><br> presso CentoxCento phone"],
+	"CornerArluno"			: [new google.maps.LatLng(45.5053578,8.942398), 		"<b>Copia&Incolla Corner</b><br> presso Mastersi"],
+	"CornerVodIntra"		: [new google.maps.LatLng(45.9362233,8.569285), 		"<b>Copia&Incolla Corner</b><br> presso Vodafone (Intra)"],
+	"CornerVodDomo"			: [new google.maps.LatLng(46.099462,8.2850653), 		"<b>Copia&Incolla Corner</b><br> presso Vodafone (Domodossola)"],
+	"CornerVodArona"		: [new google.maps.LatLng(45.7602917,8.5593297), 		"<b>Copia&Incolla Corner</b><br> presso Vodafone (Arona)"],
+	"CornerVodGrave"		: [new google.maps.LatLng(45.9314662,8.4367192), 		"<b>Copia&Incolla Corner</b><br> presso Vodafone (Gravellona)"]
 }
 
 
