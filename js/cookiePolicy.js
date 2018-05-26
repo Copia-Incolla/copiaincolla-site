3ca37d104c110077535512a9a93b76bb77796537
ABeltramo <beltramo.ale@gmail.com>
2018-05-26 10:38:17 +0200

    Added cookie policy

---
+++ js/cookiePolicy.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+        "background": "#383b75"
+        },
+        "button": {
+        "background": "#e52186"
+        }
+    },
+    "theme": "classic",
+    "position": "bottom-right",
+    "content": {
+        "message": "Questo sito non salva cookies. Utilizziamo però dei componenti esterni che potrebbero tracciare la navigazione. Per saperne di più  ",
+        "dismiss": "Ok letto!",
+        "link": "clicca qui",
+        "href": "policy.html"
+    }
+    })
+});
